adb267076eac30e369c587ff8c496c34d18385ba
Margo <lurkingsloth@gmail.com>
2018-01-20 19:24:28 +0300

    removed csrf, new HomeControllerSpec

--- fibonacci/test/HomeControllerSpec.scala
+++ fibonacci/test/HomeControllerSpec.scala
@@ -4,33 +4,39 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import javax.inject._
+
+import play.api.mvc.MessagesControllerComponents
+import InputForm._
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
+  * Add your spec here.
+  * You can mock out a whole application including requests, plugins etc.
+  *
+  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
+  */
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
+  private val sequence = scala.collection.mutable.ArrayBuffer[Int]()
+  private val postUrl = routes.HomeController.createWidget()
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
+    /*"render the index page from a new instance of controller" in {
+      val controller = new HomeController(MessagesControllerComponents)
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("Welcome to Play")
-    }
+    }*/
 
     "render the index page from the application" in {
       val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val home = controller.listWidgets().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Fibonacci")
     }
 
     "render the index page from the router" in {
@@ -39,7 +45,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Fibonacci")
     }
   }
 }
