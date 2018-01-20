95cdc30be759b57b291dc80df847d70c119bd256
Margo <lurkingsloth@gmail.com>
2018-01-20 22:35:32 +0300

    added tests

--- fibonacci/test/HomeControllerSpec.scala
+++ fibonacci/test/HomeControllerSpec.scala
@@ -1,14 +1,17 @@
 package controllers
 
-import org.scalatestplus.play._
+import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
-import javax.inject._
-
-import play.api.mvc.MessagesControllerComponents
-import InputForm._
-
+import play.api.http.Status
+import org.scalatest.concurrent.ScalaFutures
+import scala.concurrent.Future
+import org.scalatestplus.play.PlaySpec
+import play.api.http.Status
+import play.api.mvc._
+import play.api.test.Helpers._
+import play.api.test._
 /**
   * Add your spec here.
   * You can mock out a whole application including requests, plugins etc.
@@ -19,18 +22,9 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
   private val sequence = scala.collection.mutable.ArrayBuffer[Int]()
   private val postUrl = routes.HomeController.createWidget()
-  "HomeController GET" should {
+  "HomeController" should {
 
-    /*"render the index page from a new instance of controller" in {
-      val controller = new HomeController(MessagesControllerComponents)
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }*/
-
-    "render the index page from the application" in {
+    "render the index page from the application(GET)" in {
       val controller = inject[HomeController]
       val home = controller.listWidgets().apply(FakeRequest(GET, "/"))
 
@@ -39,7 +33,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       contentAsString(home) must include ("Fibonacci")
     }
 
-    "render the index page from the router" in {
+    "render the index page from the router(GET)" in {
       val request = FakeRequest(GET, "/")
       val home = route(app, request).get
 
@@ -47,5 +41,34 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("Fibonacci")
     }
+
+/*    "process a POST request successfully" in {
+      // Pull the controller from the already running Play application, using Injecting
+      val controller = inject[HomeController]
+
+      // Call using the FakeRequest and the correct body information and CSRF token
+      val request = FakeRequest(routes.HomeController.createWidget())
+        .withFormUrlEncodedBody("length" -> "17")
+        //.withCSRFToken
+      val futureResult: Future[Result] = controller.createWidget().apply(request)
+
+      // And we can get the results out using Scalatest's "Futures" trait, which gives us whenReady
+      whenReady(futureResult) { result =>
+        result.header.headers(LOCATION) must equal(routes.WidgetController.listWidgets().url)
+      }
+    }
+*/
+    "reject a POST request when given bad input" in {
+      val controller = inject[HomeController]
+
+      // Call the controller with negative price...
+      val request = FakeRequest(routes.HomeController.createWidget())
+        .withFormUrlEncodedBody("length" -> "0")
+
+      val futureResult: Future[Result] = controller.createWidget().apply(request)
+
+      status(futureResult) must be(Status.BAD_REQUEST)
+    }
+
   }
 }
---
+++ fibonacci/test/tests.scala
@@ -0,0 +1,82 @@
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.mvc.Request
+import play.api.test._
+import play.api.test.Helpers._
+import controllers.InputForm
+import play.api.data.FormError
+import play.api.i18n._
+import play.api.mvc._
+import play.api.test._
+class tests extends PlaySpec{
+
+  "inputForm" must {
+
+    "apply successfully from request" in {
+      // The easiest way to test a form is by passing it a fake request.
+      val call = controllers.routes.HomeController.createWidget()
+      implicit val request: Request[_] = FakeRequest(call).withFormUrlEncodedBody("length" -> "37")
+      // A successful binding using an implicit request will give you a form with a value.
+      val boundForm = InputForm.inputform.bindFromRequest()
+      // You can then get the widget data out and test it.
+      val data = boundForm.value.get
+      data.length must equal(37)
+    }
+
+    "apply successfully from map" in {
+      // You can also bind directy from a map, if you don't have a request handy.
+      val inp = Map("length" -> "12")
+      // A successful binding will give you a form with a value.
+      val boundForm = InputForm.inputform.bind(inp)
+      // You can then get the widget data out and test it.
+      val data = boundForm.value.get
+      data.length must equal(12)
+    }
+
+    "show errors when applied unsuccessfully(-3)" in {
+      val inp = Map("length" -> "-3")
+      val errorForm = InputForm.inputform.bind(inp)
+      val listOfErrors = errorForm.errors
+      val formError: FormError = listOfErrors.head
+      formError.key must equal("length")
+      errorForm.hasGlobalErrors mustBe false
+      formError.message must equal("error.min")
+      val lang: Lang = Lang.defaultLang
+      val messagesApi: MessagesApi = new DefaultMessagesApi(Map(lang.code -> Map("error.min" -> "Must be greater or equal to {0}")))
+      val messagesProvider: MessagesProvider = messagesApi.preferred(Seq(lang))
+      val message: String = Messages(formError.message, formError.args: _*)(messagesProvider)
+      message must equal("Must be greater or equal to 2")
+    }
+
+    "show errors when applied unsuccessfully(1)" in {
+      val inp = Map("length" -> "1")
+      val errorForm = InputForm.inputform.bind(inp)
+      val listOfErrors = errorForm.errors
+      val formError: FormError = listOfErrors.head
+      formError.key must equal("length")
+      errorForm.hasGlobalErrors mustBe false
+      formError.message must equal("error.min")
+      val lang: Lang = Lang.defaultLang
+      val messagesApi: MessagesApi = new DefaultMessagesApi(Map(lang.code -> Map("error.min" -> "Must be greater or equal to {0}")))
+      val messagesProvider: MessagesProvider = messagesApi.preferred(Seq(lang))
+      val message: String = Messages(formError.message, formError.args: _*)(messagesProvider)
+      message must equal("Must be greater or equal to 2")
+    }
+
+    "show errors when applied unsuccessfully(48)" in {
+      val inp = Map("length" -> "48")
+      val errorForm = InputForm.inputform.bind(inp)
+      val listOfErrors = errorForm.errors
+      val formError: FormError = listOfErrors.head
+      formError.key must equal("length")
+      errorForm.hasGlobalErrors mustBe false
+      formError.message must equal("error.max")
+      val lang: Lang = Lang.defaultLang
+      val messagesApi: MessagesApi = new DefaultMessagesApi(Map(lang.code -> Map("error.max" -> "Must be less or equal to {0}")))
+      val messagesProvider: MessagesProvider = messagesApi.preferred(Seq(lang))
+      val message: String = Messages(formError.message, formError.args: _*)(messagesProvider)
+      message must equal("Must be less or equal to 47")
+    }
+
+  }
+}
